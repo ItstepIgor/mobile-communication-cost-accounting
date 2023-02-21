5d1ed3c7d9cd4f897d5ab34b35b433a9ce9fc1a0
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2023-02-21 15:30:11 +0300

    Added data base in calculate-service

---
+++ calculate-service/src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -0,0 +1,41 @@
+--liquibase formatted sql
+
+--changeset igor:1
+
+ALTER TABLE IF EXISTS owner
+    ADD CONSTRAINT FK_owner_number FOREIGN KEY (number_id) REFERENCES number;
+
+ALTER TABLE IF EXISTS owner
+    ADD CONSTRAINT FK_owner_position FOREIGN KEY (position_id) REFERENCES position;
+
+ALTER TABLE IF EXISTS number
+    ADD CONSTRAINT FK_number_owner_group FOREIGN KEY (group_id) REFERENCES owner_group;
+
+ALTER TABLE IF EXISTS rule_one_time_call_service
+    ADD CONSTRAINT FK_rule_one_time_call_service_one_time_call_service FOREIGN KEY (one_time_call_service_id)
+        REFERENCES one_time_call_service;
+
+ALTER TABLE IF EXISTS number_rule_one_time_call_service
+    ADD CONSTRAINT FK_number_rule_one_time_call_service_number FOREIGN KEY (number_id) REFERENCES number;
+
+ALTER TABLE IF EXISTS number_rule_one_time_call_service
+    ADD CONSTRAINT FK_number_rule_one_time_call_service_rule_one_time_call_service FOREIGN KEY (rule_id)
+        REFERENCES rule_one_time_call_service;
+
+ALTER TABLE IF EXISTS monthly_call_service_cost
+    ADD CONSTRAINT FK_monthly_call_service_cost_monthly_call_service FOREIGN KEY (monthly_call_service_id)
+        REFERENCES monthly_call_service;
+
+ALTER TABLE IF EXISTS number_monthly_call_service
+    ADD CONSTRAINT FK_number_monthly_call_service_monthly_call_service FOREIGN KEY (monthly_call_service_id)
+        REFERENCES monthly_call_service;
+
+ALTER TABLE IF EXISTS number_monthly_call_service
+    ADD CONSTRAINT FK_number_monthly_call_service_number FOREIGN KEY (number_id)
+        REFERENCES number;
+
+ALTER TABLE IF EXISTS result
+    ADD CONSTRAINT FK_result_number FOREIGN KEY (number_id) REFERENCES number;
+
+ALTER TABLE IF EXISTS individual_result
+    ADD CONSTRAINT FK_individual_result_number FOREIGN KEY (number_id) REFERENCES number;
