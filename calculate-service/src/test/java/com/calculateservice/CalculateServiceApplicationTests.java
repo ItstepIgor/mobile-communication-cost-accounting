0b37eb4a0eae093c286ce75004fa6cfe7e8b3275
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2023-02-21 15:31:06 +0300

    Added data base in calculate-service

---
+++ calculate-service/src/test/java/com/calculateservice/CalculateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
