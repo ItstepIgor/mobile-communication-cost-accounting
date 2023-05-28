94669e5f6a441207600ccb5583a80033a594594e
igor <bim-all@yandex.ru>
2023-05-28 14:29:12 +0300

    added monthly service to individual result

--- calculate-service/src/main/java/com/calculateservice/service/impl/IndividualResultServiceImpl.java
+++ calculate-service/src/main/java/com/calculateservice/service/impl/IndividualResultServiceImpl.java
@@ -23,7 +23,11 @@ public class IndividualResultServiceImpl implements IndividualResultService {
 
     private final RuleService ruleService;
 
+    private final MonthlyCallServiceService monthlyCallServiceService;
+
     private final PhoneNumberService phoneNumberService;
+
+    private final LandlineNumberService landlineNumberService;
 
     private static final String PAID_CALL = "Платно";
     private static final String DAY_CALL = "Днем";
@@ -33,16 +37,41 @@ public class IndividualResultServiceImpl implements IndividualResultService {
     public void calcIndividualResult(LocalDate date, String number) {
         individualResultRepository.deleteAll();
         PhoneNumber phoneNumber = phoneNumberService.findPhoneNumberByNumber(Long.valueOf(number));
-        long phoneId = phoneNumber.getGroupNumber().getId();
+        long phoneGroupId = phoneNumber.getGroupNumber().getId();
+        List<String> stringListLandlineNumber = landlineNumberService.findAllLandlineNumber()
+                .stream().map(LandlineNumber::getNumber).toList();
         List<RuleOneTimeService> ruleOneTimeServices = ruleService.findRuleOneTimeService(Long.parseLong(number));
         List<IndividualResult> individualResults = new ArrayList<>();
         List<Call> allCallByNumber = callService.findAllCallByNumber(date, number);
         List<TransferWorkDay> transferWorkDays = transferWorkDayService.findAllTransferWorkDay(date);
-        if (!ruleOneTimeServices.isEmpty()) {
+        List<String> listMonthlyCallServiceName = ruleService
+                .findRuleMonthlyService(phoneNumber.getId())
+                .stream()
+                .map(RuleMonthlyService::getMonthlyCallServiceName)
+                .toList();
+        List<MonthlyCallService> monthlyCallServiceByDate = monthlyCallServiceService.findAllByDate(date);
+
+        monthlyCallServiceByDate.stream()
+                .filter(monthlyCallService -> monthlyCallService.getPhoneNumber().getNumber() == (phoneNumber.getNumber()))
+                .filter(monthlyCallService -> !listMonthlyCallServiceName
+                        .contains(monthlyCallService.getMonthlyCallServiceName()))
+                .forEach(monthlyCallService -> individualResults
+                        .add(individualResultMonthlyBuilder(phoneNumber, monthlyCallService, PAID_CALL)));
+
+        monthlyCallServiceByDate.stream()
+                .filter(monthlyCallService -> monthlyCallService.getPhoneNumber().getNumber() == (phoneNumber.getNumber()))
+                .filter(monthlyCallService -> listMonthlyCallServiceName
+                        .contains(monthlyCallService.getMonthlyCallServiceName()))
+                .forEach(monthlyCallService -> individualResults
+                        .add(individualResultMonthlyBuilder(phoneNumber, monthlyCallService, FREE_CALL)));
+
+
+        if ((!ruleOneTimeServices.isEmpty()) && (phoneGroupId == 1 || phoneGroupId == 5 || phoneGroupId == 8)) {
             for (RuleOneTimeService ruleOneTimeService : ruleOneTimeServices) {
                 allCallByNumber.stream()
                         .filter(call -> Filters.getFilterByRule(ruleOneTimeService, call))
                         .filter(call -> !Filters.getFilterByDay(transferWorkDays, call))
+                        .filter(call -> !stringListLandlineNumber.contains(call.getNumber()))
                         .filter(call -> (call.getCallDateTime().toLocalTime().isAfter(ruleOneTimeService.getStartPayment()))
                                 && (call.getCallDateTime().toLocalTime().isBefore(ruleOneTimeService.getEndPayment())))
                         .forEach(call -> individualResults.add(individualResultBuilder(phoneNumber, call, DAY_CALL)));
@@ -50,16 +79,26 @@ public class IndividualResultServiceImpl implements IndividualResultService {
                         .filter(call -> Filters.getFilterByRule(ruleOneTimeService, call))
                         .filter(call -> !Filters.getFilterByDay(transferWorkDays, call))
                         .filter(call -> Filters.getFilterByTime(ruleOneTimeService, call))
+                        .filter(call -> !stringListLandlineNumber.contains(call.getNumber()))
                         .forEach(call -> individualResults.add(individualResultBuilder(phoneNumber, call, PAID_CALL)));
                 allCallByNumber.stream()
                         .filter(call -> Filters.getFilterByRule(ruleOneTimeService, call))
                         .filter(call -> Filters.getFilterByDay(transferWorkDays, call))
+                        .filter(call -> !stringListLandlineNumber.contains(call.getNumber()))
                         .forEach(call -> individualResults.add(individualResultBuilder(phoneNumber, call, PAID_CALL)));
+                allCallByNumber.stream()
+                        .filter(call -> stringListLandlineNumber.contains(call.getNumber()))
+                        .forEach(call -> individualResults.add(individualResultBuilder(phoneNumber, call, FREE_CALL)));
             }
         } else {
-            if (phoneId == 2 || phoneId == 4 || phoneId == 6 || phoneId == 7) {
-                allCallByNumber.forEach(call -> individualResults.add(individualResultBuilder(phoneNumber, call, PAID_CALL)));
-            } else if (phoneId == 3) {
+            if (phoneGroupId == 2 || phoneGroupId == 4 || phoneGroupId == 6 || phoneGroupId == 7) {
+                allCallByNumber.stream()
+                        .filter(call -> !stringListLandlineNumber.contains(call.getNumber()))
+                        .forEach(call -> individualResults.add(individualResultBuilder(phoneNumber, call, PAID_CALL)));
+                allCallByNumber.stream()
+                        .filter(call -> stringListLandlineNumber.contains(call.getNumber()))
+                        .forEach(call -> individualResults.add(individualResultBuilder(phoneNumber, call, FREE_CALL)));
+            } else if (phoneGroupId == 3) {
                 allCallByNumber.forEach(call -> individualResults.add(individualResultBuilder(phoneNumber, call, FREE_CALL)));
             }
         }
@@ -72,6 +111,17 @@ public class IndividualResultServiceImpl implements IndividualResultService {
                 .callService(call.getCallService())
                 .phoneNumber(phoneNumber)
                 .callDateTime(call.getCallDateTime())
+                .sum(call.getSum())
+                .callType(callType)
+                .build();
+    }
+
+    private IndividualResult individualResultMonthlyBuilder(PhoneNumber phoneNumber, MonthlyCallService call, String callType) {
+        return IndividualResult.builder()
+                .ownerName(String.valueOf(call.getPhoneNumber().getNumber()))
+                .callService(call.getMonthlyCallServiceName())
+                .phoneNumber(phoneNumber)
+                .callDateTime(call.getInvoiceDate().atStartOfDay())
                 .sum(call.getSum())
                 .callType(callType)
                 .build();
