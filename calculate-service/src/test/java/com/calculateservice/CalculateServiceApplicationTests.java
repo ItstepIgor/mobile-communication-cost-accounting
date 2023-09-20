97d3968dd3e6139d7ae9b65896519bce5b04d148
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2023-09-20 16:32:22 +0300

    added first integration test

--- calculate-service/src/test/java/com/calculateservice/CalculateServiceApplicationTests.java
+++ calculate-service/src/test/java/com/calculateservice/CalculateServiceApplicationTests.java
@@ -1,14 +1,15 @@
 //package com.calculateservice;
 //
 //import org.junit.jupiter.api.Test;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.boot.test.context.TestConfiguration;
 //
 //@SpringBootTest
-//@TestConfiguration
+//@SpringBootApplication
 //public class CalculateServiceApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
+//    public static void main(String[] args) {
+//        SpringApplication.run(CalculateServiceApplicationTests.class, args);
 //    }
 //}
