f8de2a39c104ffc84ca2155ed2ed5b4fa8f9d9cf
ItstepIgor <83533310+ItstepIgor@users.noreply.github.com>
2024-05-02 16:56:18 +0300

    added address hostname for swagger servers

---
+++ import-service/src/main/java/com/importservice/configuration/OpenApiConfiguration.java
@@ -0,0 +1,23 @@
+package com.importservice.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class OpenApiConfiguration {
+
+    @Value("${spring.docker.host}")
+    private String swaggerServer;
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addServersItem(new Server().url(swaggerServer))
+                .info(new Info().title("OpenAPI definition").version("V0"));
+    }
+
+}
